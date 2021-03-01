e06da64e63908d3063e456677c15d432099c1b67
Sandra Chung <sandra@moveon.org>
2021-03-01 11:34:01 -0700

    refactored as a factory

---
+++ parser/__init__.py
@@ -0,0 +1,37 @@
+import os
+import settings
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+    app.config.from_mapping(
+        SECRET_KEY=settings.SECRET_KEY,
+        DATABASE=os.path.join(app.instance_path, 'parser.sqlite'),
+        MAX_CONTENT_LENGTH=settings.MAX_CONTENT_LENGTH,
+        UPLOAD_FOLDER=os.path.join(APP_ROOT, settings.UPLOAD_FOLDER)
+    )
+
+    from . import db
+    db.init_app(app)
+
+    from . import main
+    app.register_blueprint(main.bp)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
--- parser/main.py
+++ parser/main.py
@@ -1,21 +1,20 @@
-from flask import (flash, Flask, g, redirect, render_template, request,
-    send_from_directory, url_for)
-from werkzeug.utils import secure_filename
-
+import functools
 import csv
 import os
 import settings
 import sqlite3
 
-# configure app
-app = Flask(__name__)
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-app.config['UPLOAD_FOLDER'] = os.path.join(APP_ROOT, 'Projects/NLP/SMS_Annotation/Input_data')
+from flask import (Blueprint, flash, Flask, g, redirect, render_template, request,
+    send_from_directory, url_for)
+from parser.db import get_db
+from werkzeug.utils import secure_filename
 
-ALLOWED_EXTENSIONS = settings.ALLOWED_EXTENSIONS
-app.secret_key = settings.SECRET_KEY
-app.config['MAX_CONTENT_LENGTH'] = settings.MAX_CONTENT_LENGTH
-REQUIRED_HEADERS = settings.REQUIRED_HEADERS
+bp = Blueprint('main', __name__, url_prefix='')
+
+ALLOWED_EXTENSIONS = {'csv'}
+REQUIRED_HEADERS = {
+    'vec_file': ['names']
+}
 
 # check filename extension on uploaded files
 def allowed_file(file):
@@ -46,7 +45,7 @@ def delete_file(filename):
     return
 
 # most of the business is here
-@app.route('/', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # make sure there's a REQUIRED_HEADERS entry for each file upload input
@@ -76,8 +75,8 @@ def index():
     # GET
     return render_template('upload_form.html')
 
-# returning uploaded files
-@app.route('/uploads/<filename>')
+# returning uploaded files; get rid of this in prod
+@bp.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
